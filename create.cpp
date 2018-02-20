161f828880526dc7800de215ad81e9e4640942dd
santhoshpro05 <36360453+santhoshpro05@users.noreply.github.com>
2018-02-20 17:54:06 +0530

    Create create.cpp

---
+++ create.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int sum=0, i,N,k,a[N];
+cout<<"enter the value of k,N";
+cin>>k>>N;
+for(i=0;i<=k;i++)
+{
+sum=sum+i;
+cout<<"sum is="<<sum<<endl;
+cin>>sum;
+}
+}
