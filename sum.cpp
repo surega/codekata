c346a15a0f68323bc923137a3b1d1f36ffe32e68
santhoshpro05 <36360453+santhoshpro05@users.noreply.github.com>
2018-02-20 17:51:00 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int sum=0, i,n;
+cout<<"enter the value of n";
+cin>>n;
+for(i=0;i<=n;i++)
+{
+sum=sum+i;
+cout<<"sum is="<<sum<<endl;
+cin>>sum;
+}
+}
