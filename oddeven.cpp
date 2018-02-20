6b98e27d38f8e7945e6de1a4cf9fd169afea5555
santhoshpro05 <36360453+santhoshpro05@users.noreply.github.com>
2018-02-20 17:09:55 +0530

    Create oddeven.cpp

---
+++ oddeven.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+void display(int n)
+{
+if(n%2==0)
+cout<<"even";
+else
+cout<<"odd";
+}
+};
+int main()
+{
+A a;
+a.display(5);
+return 0;
+}
