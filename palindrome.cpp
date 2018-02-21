37e660a56726b260b7e08f529bcccb1507362de4
santhoshpro05 <36360453+santhoshpro05@users.noreply.github.com>
2018-02-21 17:22:29 +0530

    Create palindrome.cpp

---
+++ palindrome.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,digit,rev=0;
+cout<<"enter n"<<endl;
+cin>>n;
+do
+{
+digit=n%10;
+rev=(rev*10)+digit;
+n=n/10;
+}
+while(n!=0);
+if(n==rev)
+cout<<"palindrome";
+else
+cout<<"not palindrome;
+}
