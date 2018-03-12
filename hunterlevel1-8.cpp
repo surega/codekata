58a31fedbe395bbb10c0976a25f6d675e7f0aaca
surega <36365913+surega@users.noreply.github.com>
2018-03-12 17:50:08 +0530

    Create hunterlevel1-8.cpp

---
+++ hunterlevel1-8.cpp
@@ -0,0 +1,16 @@
+# include <stdio.h>
+int main()
+{
+    int l, r;
+     quickSort(A, 0, arr_size-1);
+     r = arr_size-1; 
+    while (l < r)
+    {
+         if(A[l] + A[r] == sum)
+              return 1; 
+         else if(A[l] + A[r] < sum)
+              l++;
+         else 
+              r--;
+    }    
+    return 0;
